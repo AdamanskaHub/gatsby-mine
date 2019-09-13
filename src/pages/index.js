f50b9bb4f738645e228bdcb349765370350635e4
Emilie G <adamanska@gmail.com>
2019-09-13 23:02:22 +0200

    Link added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
     return (
         <div>
             <h1>Hello</h1>
             <h2>Hello again</h2>
+            <p>Need a dev? <Link to='/contact'>Contact me</Link></p>
         </div>
     )
 }
