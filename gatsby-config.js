6b8bb362bc50d85b32f2129fb388ebbe9495bd65
Emilie G <adamanska@gmail.com>
2019-09-14 20:59:14 +0200

    first graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 /**
  * Configure your Gatsby site with this file.
- *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ * 
+ * localhost:8000/___graphql
  */
 
 module.exports = {
+
+  siteMetadata: {
+    title: 'Emilie codes',
+    author: 'Emilie'
+  },
+
   plugins: [
     'gatsby-plugin-sass'
   ]
