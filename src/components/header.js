bef80488d9e4aecba05264d9faef852313e24e95
Emilie G <adamanska@gmail.com>
2019-09-14 20:17:36 +0200

    nav improved

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+// import './header.module.scss'
+import headerStyles from './header.module.scss'
+
 const Header = () => {
     return (
-        <header>
+        <header className={headerStyles.header}> 
+            <h1><Link to='/' className={headerStyles.title}> My website</Link></h1>
             <nav>
-                <ul>
-                    <li><Link to='/'>Main</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
+                <ul className={headerStyles.navList}>
+                    <li><Link to='/blog' className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Blog</Link></li>
+                    <li><Link to='/about' className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>About</Link></li>
+                    <li><Link to='/contact' className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Contact</Link></li>
                 </ul>
             </nav>
         </header>
