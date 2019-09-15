de47164dfc5f12ac4b513aa6937245bb2b4a2a06
Emilie G <adamanska@gmail.com>
2019-09-15 19:20:21 +0200

    fetch the blog info part 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
 
   plugins: [
     'gatsby-plugin-sass',
+    'gatsby-transformer-remark',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
--- src/posts/react.md
+++ src/posts/react.md
@@ -1,7 +1,7 @@
 ---
 
-title: "The gatsby"
-date: "Yesterday"
+title: "The react post"
+date: "Someday"
 
 ---
 
