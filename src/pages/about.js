02683eb905578c67f75ad7bf95ddeb62127d0e13
Emilie G <adamanska@gmail.com>
2019-09-15 08:59:44 +0200

    Playground installed

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 const AboutPage = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    author
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
             <h1>About</h1>
-            <p>My bio</p>
+            <p>I'm {data.site.siteMetadata.author} and this is my bio</p>
             <p>Need a dev? <Link to='/contact'>Contact me</Link></p>
         </Layout>
     )
