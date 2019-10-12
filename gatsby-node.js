1826753167a18b2666e43009ee32ea09047b0897
Emilie G <adamanska@gmail.com>
2019-10-12 18:31:14 +0200

    contentful connected in part

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,6 @@
 
 const path = require('path')
 
-module.exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField} = actions
-
-    if(node.internal.type === 'MarkdownRemark') {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        // console.log(JSON.stringify(node, undefined, 4))
-
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-    
-}
 
 module.exports.createPages = async ({ graphql, actions }) => {
 
@@ -29,24 +14,22 @@ module.exports.createPages = async ({ graphql, actions }) => {
   // Get markdown data
   const res = await graphql(`
     query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
+      allContentfulBlogPost {
+        edges{
+          node{
+            slug
           }
         }
       }
     }
   `)
 
-  res.data.allMarkdownRemark.edges.forEach((edge)=>{
+  res.data.allContentfulBlogPost.edges.forEach((edge)=>{
       createPage ({
           component: blogTemplate,
-          path: `/blog/${edge.node.fields.slug}`,
+          path: `/blog/${edge.node.slug}`,
           context: {
-              slug: edge.node.fields.slug
+              slug: edge.node.slug
           }
       })
   })
